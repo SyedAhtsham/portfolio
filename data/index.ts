9d95106a1a3cedd67cd8b6b4cdda4fc008d923ec
Syed Ahtsham Ul Hassan <syed.hassan@uni-potsdam.de>
2024-08-09 16:53:28 -0400

    Remove the abosulte paths and added relative paths

--- data/index.ts
+++ data/index.ts
@@ -14,7 +14,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: "./b1.svg",
     spareImg: "",
   },
   {
@@ -44,8 +44,8 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
+    img: "./grid.svg",
+    spareImg: "./b4.svg",
   },
 
   {
@@ -55,8 +55,8 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
+    img: "./b5.svg",
+    spareImg: "./grid.svg",
   },
   {
     id: 6,
@@ -76,32 +76,32 @@ export const projects = [
     id: 1,
     title: "De-Tur Agency - Medical Tourism Web App",
     des: "De-Tur Medical Tourism Agency is based in Germany with affiliations in Turkey. Providing services to people in tourism and medical areas.",
-    img: "/p2.png",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    img: "./p2.png",
+    iconLists: ["/next.svg", "./tail.svg", "./ts.svg", "./stream.svg", "./c.svg"],
     link: "https://www.de-tur-agentur.com/",
   },
     {
     id: 2,
     title: "SimpleConvert - Currency Converter App",
     des: "Convert your Foreign-assets into your currency seamlessly using the SimpleConvert Leading Currency Converter.",
-    img: "/p1.png",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    img: "./p1.png",
+    iconLists: ["/re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./fm.svg"],
     link: "https://simplconvert.vercel.app/",
   },
   {
     id: 3,
     title: "Nike Outlet - Landing Page",
     des: "Showcasing the variety of shoes and popular products along with special offers at stores.",
-    img: "/p3.png",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    img: "./p3.png",
+    iconLists: ["/re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./c.svg"],
     link: "https://nike-outlet.vercel.app",
   },
   {
     id: 4,
     title: "Android - Landing Page",
     des: "Recreated the Android Home Page, a responsive web design with a Mega Footer and a Sticky Navbar.",
-    img: "/p4.png",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    img: "./p4.png",
+    iconLists: ["/next.svg", "./tail.svg", "./ts.svg", "./three.svg", "./gsap.svg"],
     link: "https://syedahtsham.github.io/androidLandingPage/",
   },
 ];
@@ -150,25 +150,25 @@ export const companies = [
     id: 2,
     name: "appwrite",
     img: "./app.svg",
-    nameImg: "/appName.svg",
+    nameImg: "./appName.svg",
   },
   {
     id: 3,
     name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
+    img: "./host.svg",
+    nameImg: "./hostName.svg",
   },
   {
     id: 4,
     name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
+    img: "./s.svg",
+    nameImg: "./streamName.svg",
   },
   {
     id: 5,
     name: "docker.",
-    img: "/dock.svg",
-    nameImg: "/dockerName.svg",
+    img: "./dock.svg",
+    nameImg: "./dockerName.svg",
   },
 ];
 
@@ -178,41 +178,41 @@ export const workExperience = [
     title: "Frontend Engineer Intern",
     desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    thumbnail: "./exp1.svg",
   },
   {
     id: 2,
     title: "Mobile App Dev - JSM Tech",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    thumbnail: "./exp2.svg",
   },
   {
     id: 3,
     title: "Freelance App Dev Project",
     desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
+    thumbnail: "./exp3.svg",
   },
   {
     id: 4,
     title: "Lead Frontend Developer",
     desc: "Developed and maintained user-facing features using modern frontend technologies.",
     className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
+    thumbnail: "./exp4.svg",
   },
 ];
 
 export const socialMedia = [
   {
     id: 1,
-    img: "/git.svg",
+    img: "./git.svg",
     link:"https://github.com/SyedAhtsham",
   },
 
   {
     id: 2,
-    img: "/link.svg",
+    img: "./link.svg",
     link:"https://www.linkedin.com/in/syedahtsham",
   },
 ];
