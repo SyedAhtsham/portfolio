094070923dc7fd8a985138fc4a032a3202d2a6df
Syed Ahtsham Ul Hassan <syed.hassan@uni-potsdam.de>
2024-08-09 15:58:50 -0400

    Write the modeul export statment missing

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,8 @@
 // next.config.mjs
-
-import { withSentryConfig } from '@sentry/nextjs';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Your Next.js config options here
     output: "export",
 };
 
-const sentryConfig = {
-    silent: true,
-    org: "your-org-slug",
-    project: "your-project-slug",
-};
-
-const additionalOptions = {
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-};
-
-export default withSentryConfig(nextConfig, sentryConfig, additionalOptions);
+export default withSentryConfig(nextConfig);
