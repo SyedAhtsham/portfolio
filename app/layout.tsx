121b5c856bb6dfc5156e0fd5e85d30e772e62ecb
Syed Ahtsham Ul Hassan <syed.hassan@uni-potsdam.de>
2024-08-09 15:28:59 -0400

    Change the absolute to relative paths, and made change to the configuration file for the app to build

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/aht-logo1.png" sizes="any" />
+        <link rel="icon" href="./aht-logo1.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
