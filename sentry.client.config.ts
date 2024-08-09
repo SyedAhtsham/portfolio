0a1eac3e6372c6377d6135c8825a7be466a04607
Syed Ahtsham Ul Hassan <syed.hassan@uni-potsdam.de>
2024-08-09 16:21:54 -0400

    Remove the Sentry example page

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,14 +5,13 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://91c008f9353fbc07ea377c88bc7cfcac@o4507749176311808.ingest.us.sentry.io/4507749189419008",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
   replaysOnErrorSampleRate: 1.0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://91c008f9353fbc07ea377c88bc7cfcac@o4507749176311808.ingest.us.sentry.io/4507749189419008",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
