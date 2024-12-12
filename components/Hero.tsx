399d12d910b6e69010a39a85ed8461c912a71f88
Syed Ahtsham Ul Hassan <syed.hassan@uni-potsdam.de>
2024-12-12 00:20:13 -0500

    Changed the Hero text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Transform Your Idea into a Conversion Website with Syed Ahtsham"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
